da755029a590b365081e87aac98befad263818df
knnsmelly <125581883+knnsmelly@users.noreply.github.com>
2023-03-31 10:50:40 +0800

    Create task2.py

---
+++ specimenpaper1/task2.py
@@ -0,0 +1,17 @@
+# Question 2
+
+# a = int(input())
+# b = int(input())
+
+# if a>b:
+#     print(int(((b-a)**2)**(1/2)))
+# else:
+#     print(int(((a-b)**2)**(1/2)))
+
+a = int(input())
+b = int(input())
+
+if a>b:
+    print(abs(b-a))
+else:
+    print(abs(a-b))
