c7db407d9d0c43ab1fe7e42b19b4547300a9c053
knnsmelly <125581883+knnsmelly@users.noreply.github.com>
2023-03-31 10:52:20 +0800

    Create task6.py

---
+++ specimenpaper1/task6.py
@@ -0,0 +1,20 @@
+# Question 6
+
+name1 = input()
+name2 = input()
+name3 = input()
+name4 = input()
+name5 = input()
+
+p = [name1, name2, name3, name4, name5]
+
+if p[0] == 'Ellie':
+    print('1st')
+elif p[1] == 'Ellie':
+    print('2nd') 
+elif p[2] == 'Ellie':
+    print('3rd')
+elif p[3] == 'Ellie':
+    print('4th')
+else:
+    print('5th')
